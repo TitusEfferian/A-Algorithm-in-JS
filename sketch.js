9ca0abc5334fe96cbff2f82506e8b5908ec6ef5d
TitusEfferian <kiritosato@gmail.com>
2017-05-30 23:46:23 +0700

    third commmit using color for debugging

--- sketch.js
+++ sketch.js
@@ -1,3 +1,14 @@
+function removeFromArray(arr,element)
+{
+  for(var i=arr.length-1;i>=0;i--)
+  {
+    if(arr[i]==element)
+    {
+      arr.splice(i,1);
+    }
+  }
+}
+
 var cols=5;
 var rows=5;
 var grid=new Array(cols);
@@ -12,17 +23,41 @@ var h;
 
 function Spot(i,j)
 {
-  this.x=i;
-  this.y=j;
+  this.i=i;
+  this.j=j;
   this.f=0;
   this.g=0;
   this.h=0;
+
+  this.neighbors=[];
 
   this.show=function(col)
   {
     fill(col);
     noStroke();
-    rect(this.x*w,this.y*h,w-1,h-1);
+    rect(this.i*w,this.j*h,w-1,h-1);
+  }
+  this.addNeighbors = function(grid)
+  {
+    var i = this.i;
+    var j = this.j;
+    if(i<cols-1)
+    {
+      this.neighbors.push(grid[i+1,j]);
+    }
+    if(i>0)
+    {
+      this.neighbors.push(grid[i-1,j]);
+    }
+    if(j<rows-1)
+    {
+      this.neighbors.push(grid[i,j+1]);
+    }
+    if(j>0)
+    {
+        this.neighbors.push(grid[i,j-1]);
+    }
+
   }
 }
 
@@ -43,6 +78,13 @@ function setup() {
       grid[i][j]=new Spot(i,j);
     }
   }
+  for(var i=0;i<cols;i++)
+  {
+    for(var j=0;j<rows;j++)
+    {
+      grid[i][j].addNeighbors(grid);
+    }
+  }
    start= grid[0][0];
    end=grid[cols-1][rows-1];
    openSet.push(start);
@@ -56,6 +98,22 @@ function setup() {
 function draw() {
   if(openSet.length>0)
   {
+    var lowestIndex=0;
+    for(var i=0;i<openSet.length;i++)
+    {
+      if(openSet[i].f<openSet[lowestIndex].f)
+      {
+        lowestIndex=i;
+      }
+    }
+    var current = openSet[lowestIndex];
+
+    if(current===end)
+    {
+      console.log("done");
+    }
+    removeFromArray(openSet,current);
+    closedSet.push(current);
     //we can keep going
   }
   else
