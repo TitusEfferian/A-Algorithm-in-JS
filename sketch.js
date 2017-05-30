4ea5c1609875fd1cf5dce5acd1546141e3534975
TitusEfferian <kiritosato@gmail.com>
2017-05-30 23:34:18 +0700

    second commit

--- sketch.js
+++ sketch.js
@@ -18,10 +18,10 @@ function Spot(i,j)
   this.g=0;
   this.h=0;
 
-  this.show=function()
+  this.show=function(col)
   {
-    fill(255);
-    stroke(0);
+    fill(col);
+    noStroke();
     rect(this.x*w,this.y*h,w-1,h-1);
   }
 }
@@ -67,7 +67,16 @@ function draw() {
   {
     for(var j=0;j<rows;j++)
     {
-      grid[i][j].show();
+      grid[i][j].show(color(255));
     }
+  }
+
+  for(var i=0;i<closedSet.length;i++)
+  {
+    closedSet[i].show(color(255,0,0));
+  }
+  for(var i=0;i<openSet.length;i++)
+  {
+    openSet[i].show(color(0,255,0));
   }
 }
